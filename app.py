1306c822f87bd3efb2a2f906dddcf595d9a9c88c
Juan Nicolas Sepulveda Arias <jn.sepulveda55@gmail.com>
2023-06-25 22:18:24 -0500

    Added a index.html alongside some python code that does something with the press of a button

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 @app.route('/api/program', methods=['GET'])
 def run_program():
     # Your Python program logic goes here
+
+    # Should change this yo an open AI API call
     response = {
         'message': 'Hello, world!'
     }
